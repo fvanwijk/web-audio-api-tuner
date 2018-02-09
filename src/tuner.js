7f4357dac60e2f14523d1b5d396f95921f15e9ce
Mike Woudenberg <mwoudenberg@xebia.com>
2018-02-09 15:19:29 +0100

    Adds autocorrelation

--- src/tuner.js
+++ src/tuner.js
@@ -28,11 +28,52 @@ navigator.getUserMedia(
   }
 );
 
+function findFundamentalFreq(buffer, sampleRate) {
+  // We use Autocorrelation to find the fundamental frequency.
+
+  // In order to correlate the signal with itself (hence the name of the algorithm), we will check two points 'k' frames away.
+  // The autocorrelation index will be the average of these products. At the same time, we normalize the values.
+  // Source: http://www.phy.mty.edu/~suits/autocorrelation.html
+  // Assuming the sample rate is 48000Hz, a 'k' equal to 1000 would correspond to a 48Hz signal (48000/1000 = 48),
+  // while a 'k' equal to 8 would correspond to a 6000Hz one, which is enough to cover most (if not all)
+  var n = 1024,
+    bestR = 0,
+    bestK = -1;
+  for (var k = 8; k <= 1000; k++) {
+    var sum = 0;
+
+    for (var i = 0; i < n; i++) {
+      sum += (buffer[i] - 128) / 128 * ((buffer[i + k] - 128) / 128);
+    }
+
+    var r = sum / (n + k);
+
+    if (r > bestR) {
+      bestR = r;
+      bestK = k;
+    }
+
+    if (r > 0.9) {
+      // Let's assume that this is good enough and stop right here
+      break;
+    }
+  }
+
+  if (bestR > 0.0025 && bestK !== 8) {
+    // The period (in frames) of the fundamental frequency is 'bestK'. Getting the frequency from there is trivial.
+    var fundamentalFreq = sampleRate / bestK;
+    return fundamentalFreq;
+  } else {
+    // We haven't found a good correlation
+    return -1;
+  }
+}
+
 function visualize(stream) {
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
 
-  analyser.fftSize = 2 ** 11;
+  analyser.fftSize = 2 ** 12;
   const bufferLength = analyser.frequencyBinCount;
 
   const scale = d3
@@ -47,19 +88,36 @@ function visualize(stream) {
     .append('g')
     .call(d3.axisBottom(scale));
 
+  console.log(bufferLength);
   const dataArray = new Uint8Array(bufferLength);
-
+  const binRange = audioCtx.sampleRate / bufferLength;
   canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  const freqElement = document.getElementById('freq');
+  const freqCorrl = document.getElementById('freq-corrl');
 
   const draw = function() {
     drawVisual = requestAnimationFrame(draw);
 
     analyser.getByteFrequencyData(dataArray);
 
+    var buffer = new Uint8Array(analyser.fftSize);
+    // See initializations in the AudioContent and AnalyserNode sections of the demo.
+    analyser.getByteTimeDomainData(buffer);
+    var fundalmentalFreq = findFundamentalFreq(buffer, audioCtx.sampleRate);
+
+    if (fundalmentalFreq !== -1) {
+      // var note = findClosestNote(fundalmentalFreq, notesArray); // See the 'Finding the right note' section.
+      // var cents = findCentsOffPitch(fundalmentalFreq, note.frequency); // See the 'Calculating the cents off pitch' section.
+      freqCorrl.innerText = `AutoCorrelated to ${fundalmentalFreq}`; // Function that updates the note on the page (see demo source code).
+      // updateCents(cents); // Function that updates the cents on the page and the gauge control (see demo source code).
+    }
+
     const peak = dataArray.reduce(
       (memo, value, index) => (value > memo.value ? { value, index } : memo),
       { value: -1, index: 0 }
     );
+
+    freq.innerText = `Peak: ${peak.index * binRange} Hz`;
 
     canvasCtx.fillStyle = 'rgb(0, 0, 0)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
@@ -71,7 +129,7 @@ function visualize(stream) {
     for (let i = 0; i < bufferLength; i++) {
       barHeight = dataArray[i];
 
-      canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + '',0,0)'';
+      canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',0,0)';
       canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
 
       x += barWidth + 1;
