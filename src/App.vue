2046b5d6f3a661904a4828f94692156fa1e063c1
Frank van Wijk <fvanwijk@xebia.com>
2018-02-26 23:33:50 +0100

    Draw table with frequencies and its harmonics

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import FrequencyTable from "./components/FrequencyTable.vue";
+import FrequencyTable from './components/FrequencyTable.vue';
 
 export default {
   name: 'app',
--- src/components/FrequencyTable.vue
+++ src/components/FrequencyTable.vue
@@ -1,23 +1,43 @@
 <template>
   <table>
     <thead>
-      <tr><th v-for="note in notes">{{note}}</th></tr>
+      <tr>
+        <th></th>
+        <th v-for="i in 6">{{i}}</th>
+      </tr>
     </thead>
     <tbody>
-      <tr>
-        <td v-for="note in notes">{{note}}</td>
+      <tr v-for="note in notes">
+        <th>{{note.name}}</th>
+        <td v-for="i in 6">{{(Math.round(note.frequency * i * 100) / 100).toFixed(2)}}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import { Note } from 'tonal';
+
 export default {
   name: 'FrequencyTable',
   data() {
     return {
-      notes: ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+      notes: new Array(88).fill().map((_, i) => ({
+        name: Note.fromMidi(21 + i),
+        frequency: Note.freq(21 + i)
+      }))
     }
   }
 };
 </script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  table td {
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    padding: 10px;
+    text-align: right;
+  }
+</style>
